ce09cc0c729ddbfeaa65448db052a16fce929157
ihsan-guc <ihsanguc.33@gmail.com>
2020-07-15 22:27:17 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("https://www.hepsiburada.com/poco-f2-pro-128-gb-poco-turkiye-garantili-p-HBV00000UPERS")
+print(r.status_code) # Eğer Veri Geldiyse 200 değeri döner
+
