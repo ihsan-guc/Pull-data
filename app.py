e9dd1fa23d601fc18d6fda4a7f248cc2e2e186ab
ihsan-guc <ihsanguc.33@gmail.com>
2020-07-15 22:44:40 +0300

    commit -m "Düzenleme"

--- app.py
+++ app.py
@@ -5,5 +5,12 @@ from bs4 import BeautifulSoup
 r = requests.get("https://www.hepsiburada.com/poco-f2-pro-128-gb-poco-turkiye-garantili-p-HBV00000UPERS")
 print(r.status_code) # Eğer Veri Geldiyse 200 değeri döner
 
-print(r.content) # Sistedeki var olan verileri döner 
+# print(r.content) # Sistedeki var olan verileri döner 
+
+soup = BeautifulSoup(r.content,"lxml")
+comments = soup.find_all("div",attrs={"class":"ReviewCard-module-34AJ_"})
+
+for comment in comments:
+    print(comment.find("div",attrs={"class":"ReviewCard-module-2dVP9"}).strong.string)
+    # print(comment.find("strong"))
 
