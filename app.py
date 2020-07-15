e3ea7ccce06488edb206e80f8914d0c43fb99caa
ihsan-guc <ihsanguc.33@gmail.com>
2020-07-15 22:32:31 +0300

    commit -m "düzenleme"

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+# Hepsiburada sistesinde yorum çekme
 import requests
 from bs4 import BeautifulSoup
 
 r = requests.get("https://www.hepsiburada.com/poco-f2-pro-128-gb-poco-turkiye-garantili-p-HBV00000UPERS")
 print(r.status_code) # Eğer Veri Geldiyse 200 değeri döner
+
+print(r.content) # Sistedeki var olan verileri döner 
 
