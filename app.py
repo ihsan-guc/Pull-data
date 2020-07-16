619acd1761f0709e6ad783737bb9fbeb23c27576
ihsan-guc <ihsanguc.33@gmail.com>
2020-07-16 23:02:20 +0300

    Comment Pull Data

--- app.py
+++ app.py
@@ -2,16 +2,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get("https://www.hepsiburada.com/poco-f2-pro-128-gb-poco-turkiye-garantili-p-HBV00000UPERS")
-print(r.status_code) # Eğer Veri Geldiyse 200 değeri döner
+# r = requests.get("https://www.hepsiburada.com/poco-f2-pro-128-gb-poco-turkiye-garantili-p-HBV00000UPERS")
+# print(r.status_code) # Eğer Veri Geldiyse 200 değeri döner
 
 # print(r.content) # Sistedeki var olan verileri döner 
 
-soup = BeautifulSoup(r.content,"lxml")
-comments = soup.find_all("div",attrs={"class":"ReviewCard-module-34AJ_"})
 
-for comment in comments:
-    # Şimdilik sadece yorumları kim yaptı ise onların isimlerini çektik.
-    print(comment.find("div",attrs={"class":"ReviewCard-module-2dVP9"}).strong.string) 
-    # print(comment.find("strong"))
-
+i = 1;
+j = 1;
+while i < 5:
+    r = requests.get("https://www.hepsiburada.com/poco-f2-pro-128-gb-poco-turkiye-garantili-p-HBV00000UPERS-yorumlari?sayfa={}".format(i))
+    soup = BeautifulSoup(r.content,"lxml")
+    comments = soup.find_all("div",attrs={"class":"ReviewCard-module-34AJ_"})
+    for comment in comments:
+        # Şimdilik sadece yorumları kim yaptı ise onların isimlerini çektik.
+        print(j)
+        j+=1
+        print(comment.find("div",attrs={"class":"ReviewCard-module-2dVP9"}).strong.string) 
+        print(comment.find("div",attrs={"class":"ReviewCard-module-2dVP9"}).span.string) 
+        # print(comment.find("strong"))
+    i+=1
