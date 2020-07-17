fbab464df9442f164876b64963d80b3c0bfceaf4
ihsan-guc <ihsanguc.33@gmail.com>
2020-07-17 23:04:33 +0300

    Comment Arrangement

--- app.py
+++ app.py
@@ -13,23 +13,24 @@ j = 1;
 size = 0;
 # Burada Belirtiğimiz değer sayfa sayısı sayfa sayısı var olan sayfa sayısından fazla ise otomatik olarak başa dönderir
 # yapılması gereken sayfa sayısına bakmak ve ona göre veri yazmak.4
-
+# Matris oluşturmamızın asıl sebebi tek tek veri girerek zaman harcamak yerine 
+# ilk matris = [['URL kısmını'][Sayfa Sayısını]] Yazdık ve ne kadar veri girerseniz o kadar çok veri çekecektir.
 matris = [
     ["https://www.hepsiburada.com/xiaomi-redmi-note-9-128-gb-xiaomi-turkiye-garantili-p-HBV00000UGU4C-yorumlari?sayfa={}",3],
-    ["https://www.hepsiburada.com/iphone-11-64-gb-p-HBV00000NSBZ6-yorumlari?sayfa={}",27],
-    ["https://www.hepsiburada.com/poco-f2-pro-128-gb-poco-turkiye-garantili-p-HBV00000UPERS-yorumlari?sayfa={}",4]]
-# while size < len(matris):
-#     while i <= matris[size][1]:
-#         r = requests.get(matris[size][0].format(i))
-#         soup = BeautifulSoup(r.content,"lxml")
-#         comments = soup.find_all("div",attrs={"class":"ReviewCard-module-34AJ_"})
-#         for comment in comments:
-#             # Şimdilik sadece yorumları kim yaptı ise onların isimlerini çektik.
-#             print("{}-) Name : {} \n \t Comment:{}".format(j,comment.find("div",attrs={"class":"ReviewCard-module-2dVP9"}).strong.string,comment.find("div",attrs={"class":"ReviewCard-module-2dVP9"}).span.string))
-#             j+=1
-#             # ilk Başta bir yorumu aldık aldıktan sonra o yorumun içinde strong ve span da yazan verileri çektik
-#             # print(comment.find("div",attrs={"class":"ReviewCard-module-2dVP9"}).strong.string) 
-#             # print(comment.find("div",attrs={"class":"ReviewCard-module-2dVP9"}).span.string) 
-#         i+=1
-#     i = 1;
-#     size+=1
+        ["https://www.hepsiburada.com/poco-f2-pro-128-gb-poco-turkiye-garantili-p-HBV00000UPERS-yorumlari?sayfa={}",4],
+    ["https://www.hepsiburada.com/iphone-11-64-gb-p-HBV00000NSBZ6-yorumlari?sayfa={}",27]]
+while size < len(matris):
+    while i <= matris[size][1]:
+        r = requests.get(matris[size][0].format(i))
+        soup = BeautifulSoup(r.content,"lxml")
+        comments = soup.find_all("div",attrs={"class":"ReviewCard-module-34AJ_"})
+        for comment in comments:
+            # Şimdilik sadece yorumları kim yaptı ise onların isimlerini çektik.
+            print("{}-) Name : {} \n \t Comment:{}".format(j,comment.find("div",attrs={"class":"ReviewCard-module-2dVP9"}).strong.string,comment.find("div",attrs={"class":"ReviewCard-module-2dVP9"}).span.string))
+            j+=1
+            # ilk Başta bir yorumu aldık aldıktan sonra o yorumun içinde strong ve span da yazan verileri çektik
+            # print(comment.find("div",attrs={"class":"ReviewCard-module-2dVP9"}).strong.string) 
+            # print(comment.find("div",attrs={"class":"ReviewCard-module-2dVP9"}).span.string) 
+        i+=1
+    i = 1;
+    size+=1
